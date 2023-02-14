dab6286c9eb3e4df4acc89a70a7da6c44e29339a
Faeez Miller <faeezmiller8@gmail.com>
2023-02-14 15:26:13 +0200

    FAEEZ

---
+++ app/db.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    HOST: "bzm19i6ampclfficc5wa-mysql.services.clever-cloud.com",
+    USER: "u18fdwytszdhz5pw",
+    PASSWORD: "z98NewhhOE0DgTb2tTVi",
+    DB: "bzm19i6ampclfficc5wa",
+    dialect: "mysql",
+
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+};
